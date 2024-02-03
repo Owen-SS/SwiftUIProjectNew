c52c4ff36e60c14aa123f2f3ad280c50c4f39e97
Owen Staddon <owen.staddon@icloud.com>
2024-02-03 18:42:43 +0000

    Landmarks IOS app first commit

---
+++ Views/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Owen Staddon on 02/02/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Views/LandmarkDetail.swift
@@ -0,0 +1,54 @@
+//
+//  LandmarkDetail.swift
+//  Landmarks
+//
+//  Created by Owen Staddon on 02/02/2024.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var landmark: Landmark
+    var body: some View {
+        ScrollView {
+            MapView(coordinate: landmark.locationCoordinates)
+                .frame(height: 300)
+
+
+            CircleImage(image: landmark.image)
+                .offset(y: -130)
+                .padding(.bottom, -130)
+
+
+            VStack(alignment: .leading) {
+                Text(landmark.name)
+                    .font(.title)
+
+
+                HStack {
+                    Text(landmark.park)
+                    Spacer()
+                    Text(landmark.state)
+                }
+                .font(.subheadline)
+                .foregroundStyle(.secondary)
+
+
+                Divider()
+
+
+                Text("About \(landmark.name)")
+                    .font(.title2)
+                Text(landmark.description)
+            }
+            .padding()
+        }
+        .navigationTitle(landmark.name)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+
+#Preview {
+    LandmarkDetail(landmark: landmarks[0])
+}
